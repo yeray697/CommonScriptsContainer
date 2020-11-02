b912ef2e0aee20f48bba9a121a0d530a5d6e9eb6
yeray697 <yeray.1997.yr@gmail.com>
2020-11-02 10:59:53 +0100

    Give logic to the script execution

--- CommonScripts/Model/Service/Job/RunScriptJob.cs
+++ CommonScripts/Model/Service/Job/RunScriptJob.cs
@@ -1,22 +1,41 @@
 ﻿using CommonScripts.Model.Pojo.Base;
 using Quartz;
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace CommonScripts.Model.Service.Job
 {
     public class RunScriptJob : IJob
     {
+        private ScriptAbs _script;
+
         public async Task Execute(IJobExecutionContext context)
         {
-            var script = GetScript(context);
-            await Console.Out.WriteLineAsync("Executing " + script.ScriptName); //ToDo
+            GetScript(context);
+            RunScript();
+            await Console.Out.WriteLineAsync("Executing " + _script.ScriptName); //ToDo
         }
 
-        private ScriptAbs GetScript(IJobExecutionContext context)
+        private void GetScript(IJobExecutionContext context)
         {
             var dataMap = context.MergedJobDataMap;
-            return (ScriptAbs)dataMap["script"];
+            _script = (ScriptAbs)dataMap["script"];
+        }
+
+        private void RunScript()
+        {
+            if (File.Exists(_script.ScriptPath))
+            {
+                var process = new Process();
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.FileName = @"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe";
+                process.StartInfo.Arguments = "-File \""+ _script.ScriptPath + "\"";
+
+                process.Start();
+                process.WaitForExit();
+            }
         }
     }
 }
---
+++ CommonScripts/Scripts/ToggleAudioDevice/Toggle-AudioDevice.ps1
@@ -0,0 +1,8 @@
+﻿# Define AudioDevice by ID
+$AudioDevice_A = "{0.0.0.00000000}.{adbbf8cc-ea02-42f0-be77-f98bbde5ae07}" #Speakers
+$AudioDevice_B = "{0.0.0.00000000}.{c9adcbc6-54ed-46d2-9d76-21663330f4a2}" #Headset
+
+# Toggle default playback device
+$DefaultPlayback = Get-AudioDevice -Playback
+If ($DefaultPlayback.ID -eq $AudioDevice_A) {Set-AudioDevice -ID $AudioDevice_B | Out-Null}
+Else {Set-AudioDevice -ID $AudioDevice_A | Out-Null}
