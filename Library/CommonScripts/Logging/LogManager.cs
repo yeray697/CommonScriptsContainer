e2ec5686ac51fd62580ad270dae86841f7762582
Yeray Ruiz <yeray.1997.yr@gmail.com>
2021-04-24 17:45:09 +0200

    Revert the log stuff from #15 (#19)

--- Library/CommonScripts/Logging/LogManager.cs
+++ Library/CommonScripts/Logging/LogManager.cs
@@ -2,21 +2,24 @@ using CommonScripts.Settings;
 using Serilog;
 using Serilog.Core;
 using Serilog.Events;
+using System.IO;
 
 namespace CommonScripts.Logging
 {
     public static class LogManager
     {
-        private const string LOG_PATH = @"logs\CommonScripts.log";
+        private const string LOG_FOLDER = @"logs";
+        private const string LOG_FILE_NAME = @"CommonScripts.log";
         private const string QUARTZ_SERILOG = "Quartz";
         private const string MICROSOFT_SERILOG = "Microsoft";
         private static LoggingLevelSwitch _levelSwitch;
         private static LogSink _consoleLogSink;
-
         public static void InstanceLogger()
         {
             _consoleLogSink = new LogSink();
             _levelSwitch = new LoggingLevelSwitch(AppSettingsManager.GetFileMinLogLevel());
+            string baseDirectory = AppSettingsManager.GetAppConfigDirectory();
+            CreateLogDirectory(baseDirectory);
             var loggerConfiguration = new LoggerConfiguration()
                 .MinimumLevel.ControlledBy(_levelSwitch)
                 .MinimumLevel.Override(QUARTZ_SERILOG, LogEventLevel.Warning)
@@ -24,10 +27,15 @@ namespace CommonScripts.Logging
 
             loggerConfiguration
                 .WriteTo.Console()
-                .WriteTo.File(LOG_PATH)
+                .WriteTo.File(Path.Combine(baseDirectory, LOG_FOLDER, LOG_FILE_NAME))
                 .WriteTo.Sink(_consoleLogSink);
 
             Log.Logger = loggerConfiguration.CreateLogger();
+        }
+        private static void CreateLogDirectory(string baseDirectory)
+        {
+            if (!Directory.Exists(Path.Combine(baseDirectory, LOG_FOLDER)))
+                Directory.CreateDirectory(Path.Combine(baseDirectory, LOG_FOLDER));
         }
         public static void ChangeMinLoggingLevel(LogEventLevel newLogLevel)
         {
